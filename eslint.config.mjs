c30930d590b719c6e5a5ae4dae4de98299b65ea7
SethyRung <rungsethyhk@gmail.com>
2025-10-18 20:51:12 +0700

    chore: refine ESLint configuration by adjusting rules and removing unused settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,15 @@
+// @ts-check
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
 export default withNuxt({
-  files: ["**/*.{ts,vue,js}"],
   rules: {
-    "vue/html-self-closing": [
-      "warn",
-      {
-        html: {
-          void: "always",
-          normal: "never",
-        },
-      },
-    ],
+    "import/first": "off",
+    "import/order": "off",
     "vue/multi-word-component-names": "off",
+    "vue/max-attributes-per-line": ["error", { singleline: 5 }],
+    "vue/html-self-closing": "off",
+    "@typescript-eslint/ban-types": "off",
+    "@typescript-eslint/no-empty-object-type": "off",
     "@typescript-eslint/no-explicit-any": "off",
   },
 });
