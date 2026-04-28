56ab527a81330a00562b9aae695400e73e4154d9
SethyRung <rungsethyhk@gmail.com>
2026-04-28 16:40:13 +0700

    feat: enhance useApi function to accept options for method, body, query, and headers

--- app/composables/useApi.ts
+++ app/composables/useApi.ts
@@ -1,14 +1,19 @@
 import { appendResponseHeader } from "h3";
 
-export async function useApi<T>(url: string) {
+export async function useApi<T>(
+  url: string,
+  options?: { method?: any; body?: any; query?: any; headers?: Record<string, string> },
+) {
   const event = useRequestEvent();
   const headers = useRequestHeaders(["cookie"]);
 
-  const res = await $fetch.raw<T>(url, { headers });
+  const res = await $fetch.raw<T>(url, {
+    ...options,
+    headers: { ...headers, ...options?.headers },
+  });
 
   if (event) {
     const cookies = res.headers.getSetCookie();
-
     for (const cookie of cookies) {
       appendResponseHeader(event, "set-cookie", cookie);
     }
