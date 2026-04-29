fda757d1e5c451e9431945ad1fc18cfca3a4eb0d
SethyRung <rungsethyhk@gmail.com>
2026-04-29 10:18:06 +0700

    feat: implement custom fetch plugin and update useApi for improved request handling and response type resolving

--- app/composables/useApi.ts
+++ app/composables/useApi.ts
@@ -1,25 +1,10 @@
-import { appendResponseHeader } from "h3";
 import type { NitroFetchRequest, NitroFetchOptions } from "nitropack";
 
-export function useApi<T>(
-  req: NitroFetchRequest,
-  opts: NitroFetchOptions<
-    NitroFetchRequest,
-    "get" | "post" | "put" | "delete" | "patch" | "options" | "head"
-  > = {},
-) {
-  const event = useRequestEvent();
-  const headers = useRequestHeaders(["cookie"]);
-
-  return $fetch<ApiResponse<T>>(req, {
-    ...opts,
-    headers: { ...headers, ...opts.headers },
-    onResponse(context) {
-      if (!event) return;
-      const cookies = context.response.headers.getSetCookie();
-      for (const cookie of cookies) {
-        appendResponseHeader(event, "set-cookie", cookie);
-      }
-    },
-  });
+export function useApi<
+  T = unknown,
+  R extends NitroFetchRequest = NitroFetchRequest,
+  O extends NitroFetchOptions<R> = NitroFetchOptions<R>,
+>(req: R, opts?: O) {
+  const nuxtApp = useNuxtApp();
+  return nuxtApp.$fetch<T, R, O>(req, opts);
 }
